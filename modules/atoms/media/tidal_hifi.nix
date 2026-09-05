ead6e81e52c1e13843dc3776b2ae9c5728ea349d
Petre Razvan <petre.ispir2002@protonmail.com>
2026-09-05 21:18:14 +0300

    tidal has a bug and needs --disable-dev-shm-usage launch command

--- modules/atoms/media/tidal_hifi.nix
+++ modules/atoms/media/tidal_hifi.nix
@@ -8,8 +8,16 @@
         inherit (pkgs.stdenv.hostPlatform) system;
         config.allowUnfreePredicate = pkg: builtins.elem (pkgs.lib.getName pkg) [ "castlabs-electron" ];
       };
+
+      tidal_hifi_shm = pkgs_unstable.tidal-hifi.overrideAttrs (old: {
+        nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [ pkgs.makeBinaryWrapper ];
+        postFixup = (old.postFixup or "") + ''
+          wrapProgram "$out/bin/tidal-hifi" \
+            --add-flags "--disable-dev-shm-usage"
+        '';
+      });
     in
     {
-      environment.systemPackages = [ pkgs_unstable.tidal-hifi ];
+      environment.systemPackages = [ tidal_hifi_shm ];
     };
 }
