e96a7d286dd01245b722717e9fd842740afd546f
Petre Razvan <petre.ispir2002@protonmail.com>
2026-09-06 17:55:39 +0300

    added some printer drivers

--- modules/system/core/printing.nix
+++ modules/system/core/printing.nix
@@ -2,14 +2,24 @@
 { ... }:
 {
   flake.nixosModules.printing =
-    { ... }:
+    { pkgs, ... }:
     {
-      services.printing.enable = true;
+      services.printing = {
+        enable = true;
+        drivers = with pkgs; [
+          gutenprint
+          hplip
+          splix
+          brlaser
+        ];
+      };
+
       services.avahi = {
         enable = false; # change to true if needed for compatible printer
         nssmdns4 = true;
         openFirewall = true;
       };
+
       services.ipp-usb.enable = false; # change to true if needed for compatible printer
     };
 }
